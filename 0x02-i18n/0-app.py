d5602101c08963db21d4baa244b02df198ee7d8b
Brian <chumobrian@gmail.com>
2024-05-14 12:41:22 +0300

    Add Flask server initialization in 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+'''Defines a Flask app module'''
+
+from flask import Flask
+
+@app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    '''returns an index HTML file'''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
