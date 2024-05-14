78d5ebf209b4918dae43d500b405b073039715a5
Brian <chumobrian@gmail.com>
2024-05-14 23:21:03 +0300

    Add Flask app server initialization script

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python3
+'''Defines a Flask app module'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    '''Configurations class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    returns local language
+
+    Returns:
+        str: Best matching locale language code
+    """
+    languages = app.config["LANGUAGES"]  # locale language code
+    locale = request.args.get('locale')
+    print(locale)
+    if locale and locale in languages:
+        return locale
+    return request.accept_languages.best_match(languages)
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    '''
+    returns an index HTML file
+
+    Returns:
+        str: Homepage content
+    '''
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
