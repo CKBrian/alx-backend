cd4c710866e7b710bd49c308fab92c0b789a1281
Brian <chumobrian@gmail.com>
2024-05-14 22:18:03 +0300

    Add Flask app server initialization script

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 class Config:
     '''Configurations class'''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
