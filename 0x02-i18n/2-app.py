0656a01cfbf0d8a984d8c30ba8fcb01e2f2d78e7
Brian <chumobrian@gmail.com>
2024-05-14 20:24:28 +0300

    Add Flask server initialization in 0-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+'''Defines a Flask app module'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    '''Configurations class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """returns local language"""
+    return request.accept_languages.best_match()
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    '''returns an index HTML file'''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
