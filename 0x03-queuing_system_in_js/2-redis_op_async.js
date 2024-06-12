c540a25573ab2188ddf3eb8a84536a632a00af74
Brian <chumobrian@gmail.com>
2024-06-12 14:31:30 +0300

    chore: Add a script that connects to a  node Redis client and peforms basic operations using Async functions

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,29 @@
+import { createClient } from 'redis';
+const redis = require('redis');
+const util = require('util');
+
+const client = createClient();
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`))
+
+client.on('ready', () => 'Redis client connected to the server');
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+}
+
+/*promisify get method*/
+const getAsync = util.promisify(client.get).bind(client);
+
+const displaySchoolValue = async (schoolName) => {
+  try {
+    const data = await getAsync (schoolName);
+	console.log(`${data}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
