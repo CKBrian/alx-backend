813ac45f6a6dd85180c79c02b584d396435c979b
Brian <chumobrian@gmail.com>
2024-06-12 13:20:06 +0300

    chore: Add a script that connects to a  node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_client.js
+++ 0x03-queuing_system_in_js/1-redis_client.js
@@ -8,12 +8,20 @@ client.on('error', err => console.log(`Redis client not connected to the server:
 client.on('ready', () => 'Redis client connected to the server');
 
 const setNewSchool = (schoolName, value) => {
-  client.set(schoolName, value);
-  redis.print;
+  client.set(schoolName, value, redis.print);
 }
 
-const displaySchoolValue = (schoolName) => console.log(`schoolName`);
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, data) => {
+    if (err) throw err;
+    if (data) console.log(`${data}`);
+  });
+}
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+const getSetData = () => {
+  displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  displaySchoolValue('HolbertonSanFrancisco');
+
+}
+getSetData();
