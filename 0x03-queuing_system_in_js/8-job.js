9a162f8ce20b68a4746188dc138c5ff56e7d7e04
Brian <chumobrian@gmail.com>
2024-06-14 11:32:49 +0300

    feat: add a function that uses a job object and queue to create a job

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,14 +1,4 @@
-/*
-In a file named 8-job.js, create a function named createPushNotificationsJobs:
 
-It takes into argument jobs (array of objects), and queue (Kue queue)
-If jobs is not an array, it should throw an Error with message: Jobs is not an array
-For each job in jobs, create a job in the queue push_notification_code_3
-When a job is created, it should log to the console Notification job created: JOB_ID
-When a job is complete, it should log to the console Notification job JOB_ID completed
-When a job is failed, it should log to the console Notification job JOB_ID failed: ERROR
-When a job is making progress, it should log to the console Notification job JOB_ID PERCENT% complete
-*/
 const createPushNotificationsJobs = (jobs, queue) => {
     if (!Array.isArray(jobs)) {
         throw new Error('Jobs is not an array');
